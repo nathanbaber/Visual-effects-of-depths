a21af8a33637f407d7ea3f3a29c8cc6e8ea9a273
nathanbaber <nathanbaber0@gmail.com>
2025-05-29 23:01:28 +0300

    Add Bubble's Particle System

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -1,44 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Fish : MonoBehaviour
-{
-    // Дополнительные параметры, которые можно настраивать в инспекторе:
+public class Fish : MonoBehaviour {
 
-    [Tooltip("Скорость анимации плавников (если используется анимация)")]
-    public float swimAnimationSpeed = 1f;
+    float speed;
+    bool turning = false;
 
-    [Tooltip("Материал рыбы (если нужно менять цвет или другие параметры)")]
-    public Material fishMaterial;
+    void Start() {
 
-    private void Start()
-    {
-        // Получаем компонент Renderer (если он есть)
-        Renderer fishRenderer = GetComponent<Renderer>();
+        speed = Random.Range(FishManager.FM.minSpeed, FishManager.FM.maxSpeed);
+    }
 
-        // Если у рыбы есть материал, сохраняем его
-        if (fishRenderer != null)
-        {
-            fishMaterial = fishRenderer.material; // Получаем экземпляр материала
+
+    void Update() {
+
+        Bounds b = new Bounds(FishManager.FM.transform.position, FishManager.FM.swimLimits * 2.0f);
+
+        if (!b.Contains(transform.position)) {
+
+            turning = true;
+        } else {
+
+            turning = false;
         }
 
-        // Дополнительная инициализация (если требуется)
+        if (turning) {
+
+            Vector3 direction = FishManager.FM.transform.position - transform.position;
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(direction),
+                FishManager.FM.rotationSpeed * Time.deltaTime);
+        } else {
+
+
+            if (Random.Range(0, 100) < 10) {
+
+                speed = Random.Range(FishManager.FM.minSpeed, FishManager.FM.maxSpeed);
+            }
+
+
+            if (Random.Range(0, 100) < 10) {
+                ApplyRules();
+            }
+        }
+
+        this.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
     }
 
-    private void Update()
-    {
-        // Если у вас есть анимация, можно управлять ею здесь,
-        // например, менять скорость анимации плавников:
-        // GetComponent<Animator>().SetFloat("Speed", swimAnimationSpeed);
+    private void ApplyRules() {
 
-        // Дополнительные обновления (если требуется)
-    }
+        GameObject[] gos;
+        gos = FishManager.FM.allFish;
 
-    // Другие функции, например, для изменения цвета, если нужно.
-    public void SetColor(Color color)
-    {
-        if (fishMaterial != null)
-        {
-            fishMaterial.color = color;
+        Vector3 vCentre = Vector3.zero;
+        Vector3 vAvoid = Vector3.zero;
+
+        float gSpeed = 0.01f;
+        float mDistance;
+        int groupSize = 0;
+
+        foreach (GameObject go in gos) {
+
+            if (go != this.gameObject) {
+
+                mDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if (mDistance <= FishManager.FM.neighbourDistance) {
+
+                    vCentre += go.transform.position;
+                    groupSize++;
+
+                    if (mDistance < 1.0f) {
+
+                        vAvoid = vAvoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Fish anotherFlock = go.GetComponent<Fish>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+
+        if (groupSize > 0) {
+
+            vCentre = vCentre / groupSize + (FishManager.FM.goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            if (speed > FishManager.FM.maxSpeed) {
+
+                speed = FishManager.FM.maxSpeed;
+            }
+
+            Vector3 direction = (vCentre + vAvoid) - transform.position;
+            if (direction != Vector3.zero) {
+
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,
+                    Quaternion.LookRotation(direction),
+                    FishManager.FM.rotationSpeed * Time.deltaTime);
+            }
         }
     }
 }
